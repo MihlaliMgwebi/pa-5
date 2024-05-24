b70330b7944baa70d251d3f77eb21cfe633d3433
mmgwebi <mihlalimgwebi@gmail.com>
2024-05-24 23:29:11 +0200

    feat: :lipstick: render list of lecturers

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+// import { FormsModule } from '@angular/forms';
+// import { RouterLink } from '@angular/router';
+// import { IonicModule } from '@ionic/angular';
+import { LecturerServiceService } from '../services/lecturer-service.service';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+  styleUrls: ['tab3.page.scss'],
+  // TODO
+  // standalone: true,
+  // imports: [IonicModule, CommonModule, FormsModule, RouterLink],
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit {
+  // create 2 variables to store results emitted from observable
+  lecturers: any;
+  newLecturers: any;
 
-  constructor() {}
+  // inject student service into the constructor
+  constructor(private lecturerService: LecturerServiceService) {}
 
+  // create a method to get the data from the json-data-students.php file
+  getLecturerData() {
+    // subscribe to the observable
+    // result is the data emitted from the observable
+    this.lecturerService.getLecturers().subscribe((result) => {
+      // store the data emitted from the observable into the lecturers variable
+      this.lecturers = result;
+      // console.log(this.lecturers);
+      // store the data emitted from the observable into the newLecturers variable
+      this.newLecturers = this.lecturers.lecturers;
+    });
+  }
+
+  // call the getLecturerData() method when the page loads
+  ngOnInit() {
+    this.getLecturerData();
+  }
 }
